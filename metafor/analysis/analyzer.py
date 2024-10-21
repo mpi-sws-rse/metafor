d0539c081a2e1da65f5ca3bbad67cbd906922282
Rupak Majumdar <rupak@mbk-20-0227.mpi-sws.org>
2024-10-21 20:54:56 +0200

    modified:   analyzer.py

--- metafor/analysis/analyzer.py
+++ metafor/analysis/analyzer.py
@@ -105,30 +105,7 @@ class Experiment:
     def show(self, _results):
         pass
 
-    def sweep(self, plist: ParameterList):
-        results = []
-        for params in plist:
-            print("Running experiment with parameters ", params)
-            program = self.build(params)
-            results.append(self.analyze(params, program))
-        print("Sweep: \n", results)
-        self.show(results)
-
-
-
-class TestProgram(Experiment):
-    def __init__(self):
-        pass
-
-    def build(self, param) -> Program:
-        apis = { 'rd': Work(10, []) }
-        s = Server("server", apis, 100, 20, 1)
-        rdsrc = Source("reader", "rd", 9.5, 9, 3)
-        p = Program("single_server")
-        p.add_server(s)
-        p.add_source(rdsrc)
-        p.connect("reader", "server")
-
+    def update(self, p: Program, param):
         server_params = param.get('server', None)
         source_params = param.get('source', None)
 
@@ -161,6 +138,31 @@ class TestProgram(Experiment):
                     if 'retries' in source_map:
                         s.retries = source_map['retries']
         return p
+
+    def sweep(self, plist: ParameterList):
+        results = []
+        for params in plist:
+            print("Running experiment with parameters ", params)
+            program = self.build(params)
+            results.append(self.analyze(params, program))
+        print("Sweep: \n", results)
+        self.show(results)
+
+
+
+class TestProgram(Experiment):
+    def __init__(self):
+        pass
+
+    def build(self, param) -> Program:
+        apis = { 'rd': Work(10, []) }
+        s = Server("server", apis, 100, 20, 1)
+        rdsrc = Source("reader", "rd", 9.5, 9, 3)
+        p = Program("single_server")
+        p.add_server(s)
+        p.add_source(rdsrc)
+        p.connect("reader", "server")
+        return self.update(p, param)
 
     def analyze(self, param_setting, p: Program):
         ctmc = p.build()
