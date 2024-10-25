ceee6c0077ba776258f4878fd5f169273b87f102
alebugariu <alexandra_bugariu@yahoo.com>
2024-10-25 22:42:30 +0200

    fixed the plotting

--- metafor/analysis/multi_server/latency_finite_time_experiment.py
+++ metafor/analysis/multi_server/latency_finite_time_experiment.py
@@ -1,5 +1,7 @@
 from typing import Any, Dict
 
+import numpy as np
+
 from analysis.experiment import Experiment
 from dsl.dsl import Source, Server, Work, Program, DependentCall, Constants
 from model.multi_server.ctmc import MultiServerCTMC
@@ -37,29 +39,39 @@ class TestLatencyFiniteTime(Experiment):
         pi = ctmc.get_init_state()
         res = ctmc.finite_time_analysis(pi, {'main queue size': ctmc.main_queue_size_analysis},
                                         sim_time=param_setting['sim_time'], sim_step=param_setting['sim_step'])
-        avg_len = [0.0]
-        var_len = [0.0]
-        std_len = [0.0]
-        runtime = [0.0]
+        avg_len = np.array([0.0] * ctmc.server_num)
+        var_len = np.array([0.0] * ctmc.server_num)
+        std_len = np.array([0.0] * ctmc.server_num)
+        runtime = np.array([0.0])
 
         for res_step in res.values():
             if 'main queue size' in res_step.keys():
                 size_res = res_step['main queue size']
                 if isinstance(size_res, dict):
-                    avg_len.append(size_res['avg'])
-                    var_len.append(size_res['var'])
-                    std_len.append(size_res['std'])
+                    avg_len = np.append(avg_len, size_res['avg'])
+                    var_len = np.append(var_len, size_res['var'])
+                    std_len = np.append(std_len, size_res['std'])
             if 'wallclock_time' in res_step.keys():
-                runtime.append(res_step['wallclock_time'])
+                runtime = np.append(runtime, res_step['wallclock_time'])
 
-        assert len(avg_len) == len(var_len) == len(std_len) == len(runtime)
+        assert len(avg_len) == len(var_len) == len(std_len) == ctmc.server_num * len(runtime)
         results = {'avg_len': avg_len, 'var_len': var_len, 'std_len': std_len, 'runtime': runtime}
         results.update(param_setting)
         return results
 
     def show(self, results):
-        plot_results(results['sim_step'], results['avg_len'], results['var_len'], results['std_len'],
-                     results['runtime'], 'latency_finite_time_' + results['file_name'])
+        avg_len = results['avg_len']
+        var_len = results['var_len']
+        std_len = results['std_len']
+        runtime = results['runtime']
+        servers = int(len(avg_len) / len(runtime))
+        for server_id in range(servers):
+            plot_results(results['sim_step'],
+                         avg_len[server_id:len(avg_len):servers],
+                         var_len[server_id:len(avg_len):servers],
+                         std_len[server_id:len(avg_len):servers],
+                         runtime,
+                         'latency_finite_time_server_' + str(server_id) + '_' + results['file_name'])
 
 
 if __name__ == "__main__":
